0fa75b62acda234a474fb75091212cec4ae2f47c
Franklin Lee <frankgoji@gmail.com>
2015-07-15 13:25:23 -0700

    vid downloader

---
+++ vid/get_links.py
@@ -0,0 +1,5 @@
+# This python script uses selenium to get the appropriate links for the
+# specified youtube playlist, then prints them out.
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
---
+++ vid/vid.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+# This script will take a youtube playlist link and download all the videos in
+# the playlist.
