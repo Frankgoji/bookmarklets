5998f5b9681fb26ea1d75c8d8f0d5d301fd1b486
Franklin Lee <frankgoji@gmail.com>
2015-07-14 20:27:36 -0700

    first commit

---
+++ downv.js
@@ -0,0 +1,6 @@
+javascript:(function(){
+    _myscript=document.createElement('SCRIPT');
+    _myscript.type='text/javascript';
+    _myscript.src='https://googledrive.com/host/0B79SR6ca7vCeNWlkb19JaHhUUUk';
+    document.getElementsByTagName('head')[0].appendChild(_myscript);
+})();
---
+++ remove.js
@@ -0,0 +1,6 @@
+javascript:(function(){
+    _my_script=document.createElement('SCRIPT');
+    _my_script.type='text/javascript';
+    _my_script.src='https://googledrive.com/host/0B79SR6ca7vCeNWtqZTREMWRkNGs';
+    document.getElementsByTagName('head')[0].appendChild(_my_script);
+})();
