d50c01e7cc38a6e50067dc4952a1781dcab74c44
Franklin Lee <frankgoji@gmail.com>
2015-07-16 18:11:29 -0700

    get_links done, vid in progress

--- vid/get_links.py
+++ vid/get_links.py
@@ -3,3 +3,24 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from sys import argv
+
+# read argument to get playlist url
+if len(argv) != 2:
+    raise ValueError("Needs only 1 arg: playlist url")
+
+pl_url = argv[1]
+
+driver = webdriver.Firefox()
+driver.get("http://downvids.net/download-youtube-playlist-videos")
+bar = driver.find_element_by_name("playlist")
+bar.send_keys(pl_url)
+bar.send_keys(Keys.RETURN)
+# close popup
+driver.switch_to_window("windowname5")
+driver.close()
+driver.switch_to_window(driver.window_handles[0])
+links = driver.find_elements_by_link_text("Download as video")
+download_urls = ";".join(map(lambda x: x.get_attribute("href"), links))
+print(download_urls)
+driver.quit()
--- vid/vid.sh
+++ vid/vid.sh
@@ -1,3 +1,20 @@
 #! /bin/bash
 # This script will take a youtube playlist link and download all the videos in
 # the playlist.
+
+function error {
+    echo $1
+    exit 1
+}
+
+if [[ $# != 1 ]]; then
+    error "Should only be one argument: the url"
+fi
+
+url=$1
+
+links=$(python3 ~/Documents/personal_projects/bookmarklets/vid/get_links.py $url)
+arr_links=(${links//;/ })
+for l in "${arr_links[@]}"; do
+    wget $l &
+done
