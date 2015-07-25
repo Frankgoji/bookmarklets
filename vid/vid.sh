5b6b580e273e6f200d3aaf3245c91d60aa21f7e4
Franklin Lee <frankgoji@gmail.com>
2015-07-25 00:17:29 -0700

    name and folder edits

--- vid/vid.sh
+++ vid/vid.sh
@@ -7,17 +7,21 @@ function error {
     exit 1
 }
 
-if [[ $# != 1 ]]; then
-    error "Should only be one argument: the url"
+if [[ $# != 2 ]]; then
+    error "There should be two arguments: the url and the groupname"
 fi
 
 url=$1
+name=$2
+
+mkdir $name
+cd $name
 
 links=$(python3 ~/Documents/personal_projects/bookmarklets/vid/get_links.py $url)
 arr_links=(${links//;/ })
 i=0
 for l in "${arr_links[@]}"; do
-    printf -v j "%02d" $i
+    printf -v j "${name}_%02d" $i
     let "i += 1"
     wget --output-document=$j $l
 done
