dc912039218b215f60071a245628b913918c9330
Franklin Lee <frankgoji@gmail.com>
2016-01-22 14:51:08 -0800

    wrote choose random video bookmarklet

---
+++ chooserandom.js
@@ -0,0 +1,16 @@
+// Chooses a random video in a youtube playlist
+javascript:(function(){
+    _my_script=document.createElement('SCRIPT');
+    _my_script.type='text/javascript';
+    _my_script.src='https://googledrive.com/host/0B79SR6ca7vCedFFteGhpSjhqcDg';
+    document.getElementsByTagName('head')[0].appendChild(_my_script);
+})();
+
+// This is the script listed in the google drive file
+function clickButton() {
+    _links = document.getElementsByClassName("pl-video-title-link yt-uix-tile-link yt-uix-sessionlink  spf-link ");
+    _link = _links[Math.floor(Math.random() * _links.length)];
+    window.location.href = _link.href;
+}
+
+clickButton();
