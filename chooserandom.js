51da3d7dae854477712442803bdb6609608442ed
Franklin Lee <frankgoji@gmail.com>
2016-02-16 18:22:20 -0800

    finalize chooserandom

--- chooserandom.js
+++ chooserandom.js
@@ -2,7 +2,7 @@
 javascript:(function(){
     _my_script=document.createElement('SCRIPT');
     _my_script.type='text/javascript';
-    _my_script.src='https://googledrive.com/host/0B79SR6ca7vCedFFteGhpSjhqcDg';
+    _my_script.src='https://googledrive.com/host/0B79SR6ca7vCeb0FqTmJKNmVXbDA';
     document.getElementsByTagName('head')[0].appendChild(_my_script);
 })();
 
