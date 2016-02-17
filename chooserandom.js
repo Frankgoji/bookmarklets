c2f085d81af8ddb5052837494a4b87de0b04d70a
Franklin Lee <frankgoji@gmail.com>
2016-02-16 18:26:50 -0800

    yep

--- chooserandom.js
+++ chooserandom.js
@@ -18,8 +18,8 @@ function clickButton() {
 
 function clickLoad() {
     _load_more = "yt-uix-button yt-uix-button-size-default yt-uix-button-default load-more-button yt-uix-load-more browse-items-load-more-button";
-    _load_button = document.getElementsByClassName(_load_more)
-    if (_load_button.length != 0) {
+    _load_button = document.getElementsByClassName(_load_more);
+    if (_load_button.length !== 0) {
         _load_button[0].click();
         setTimeout(clickLoad, 1000);
     }
