85419aa5aba040591b4f3b8af57ba23cdb45df93
Franklin Lee <frankgoji@gmail.com>
2016-09-02 09:45:59 -0700

    Starting a large playlist download module

---
+++ vid/parallel_pl.py
@@ -0,0 +1,4 @@
+# Downloads a large playlist by individually getting the video urls, using a
+# downloader, and running a parallel operation to download it
+# Differs from vid.sh in that it doesn't use a playlist downloader, which hangs
+# on large playlists
