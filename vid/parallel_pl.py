cdc1b5c2c2f659d872159ea19314bad8003ced93
Franklin Lee <frankgoji@gmail.com>
2016-09-03 00:31:40 -0700

    finished majority of parallel downloader

--- vid/parallel_pl.py
+++ vid/parallel_pl.py
@@ -16,42 +16,100 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from sys import argv
+import os
+import datetime
+import subprocess
+import threading
 
 class ParaPLDL:
     """A Parallel PlayList DownLoader"""
 
-    def __init__(self, url):
+    def __init__(self, url, name):
         """Initializes the downloader with a Selenium webdriver"""
+        # Create the log file
+        self.name = name
+        self.logfile = self.name + '.log'
+        with open(self.logfile, 'w') as l:
+            pass # initialize empty log file
+
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(20)
-        self.driver.get(url) # TODO: don't get url, get the pl download site and
-        # input the url. can get the titles from checking the elements
-        # TODO: click all the links until all of the playlist elements are
-        # present
-        #bar = driver.find_element_by_name("playlist")
-        #bar.send_keys(pl_url)
-        #bar.send_keys(Keys.RETURN)
-        #driver.switch_to_window("windowname5")
-        #driver.close()
-        #driver.switch_to_window(driver.window_handles[0])
-        #links = driver.find_elements_by_link_text("Download as video")
-        #download_urls = ";".join(map(lambda x: x.get_attribute("href"), links))
+        self.driver.get("http://downvids.net/download-youtube-playlist-videos")
+        self.log("Started Selenium driver")
 
-    def run(self, thread_count=5):
-        """Runs the download process with multiple threads"""
-        def _link_getter(driver,)
+        bar = self.driver.find_element_by_name("playlist")
+        bar.send_keys(url)
+        bar.send_keys(Keys.RETURN)
+        self.log("Inputted the playlist url")
+        self.driver.switch_to_window("windowname5")
+        self.driver.close()
+        self.log("Closed popup window")
+        self.driver.switch_to_window(self.driver.window_handles[0])
 
-    def close(self):
+        links = self.driver.find_elements_by_link_text("Download as video")
+        self.download_urls = list(map(lambda x: x.get_attribute("href"), links))
+        self.titles = list(map(lambda x: x.text,
+                    self.driver.find_elements_by_css_selector('.msgtxt.en')))
+        assert self.download_urls == self.titles, "Diff num of titles and links"
+        self.log("Acquired video download links and titles")
         self.driver.quit()
-        # TODO: maybe close the threads if necessary?
+        self.log("Closed the Selenium webdriver")
+
+    def log(self, line):
+        """Writes line to log at self.log with timestamps"""
+        now = datetime.datetime.now()
+        time = datetime.datetime.strftime(now, '(%d %b %Y %H:%M:%S)')
+        with open(self.logfile, 'a') as log:
+            log.write(time + ' ' + line + '\n')
+
+    def run(self, thread_count=2):
+        """Runs the download process with multiple threads"""
+        trycount = 10
+        numprefix = '{0:0%dd}' % len(str(thread_count))
+
+        def downloader(thread_num):
+            """A thread that runs the download"""
+            tid = 'Thread ' + numprefix.format(thread_num) + ': '
+            for i in range(thread_num, len(self.titles), thread_count):
+                title, link = self.titles[i], self.download_urls[i]
+                name = numprefix.format(thread_num) + ' ' + title + '.mp4'
+                tries = 0
+                while (not os.path.exists(name) or os.path.getsize(name) == 0) \
+                        and tries <= trycount:
+                    if os.path.exists(name): os.remove(name)
+                    subprocess.call(['wget', '--output-document=' + name, link])
+                    self.log(tid + 'Calling wget for ' + name)
+                    tries += 1
+                if (not os.path.exists(name) or os.path.getsize(name) == 0):
+                    self.log(tid + 'wget failed for ' + name)
+                else:
+                    self.log(tid + 'wget successfully downloaded ' + name)
+
+        threads = []
+        for i in range(thread_count):
+            threads.append(threading.Thread(target=downloader, args=(i,)))
+            self.log('Thread ' + numprefix.format(i) + ' created')
+
+        for i in range(thread_count):
+            threads[i].start()
+            self.log('Thread ' + numprefix.format(i) + ' started')
+
+        for t in threads: t.join()
+        self.log('Threads terminated')
+        self.log('Downloads completed')
+
 
 if __name__ == '__main__':
-    if len(argv) != 2:
-        raise ValueError("Needs only playlist url")
+    if len(argv) != 3:
+        raise ValueError("Needs playlist url and name")
 
     url = argv[1]
+    name = argv[2]
 
-    pldl = ParaPLDL(url)
+    # Create folder
+    os.mkdir(name)
+    os.chdir(name)
+
+    pldl = ParaPLDL(url, name)
     pldl.run()
-    pldl.close()
 
