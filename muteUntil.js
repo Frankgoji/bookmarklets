971d91ca479f930df54c5407c657aeb8e3d1ada5
Frankgoji <frankgoji@gmail.com>
2019-05-09 17:04:11 -0700

    fixed muteUntil bookmarklet bug where animated titles were causing the script to not mute sometimes. Keeping console logs if the bookmarklet needs to be debugged in the future.

--- muteUntil.js
+++ muteUntil.js
@@ -1,12 +1,18 @@
 javascript: (function() {
     document.getElementsByClassName('VolumeDurationControl__VolumeIconContainer')[0].click();
     _currSong = document.getElementsByClassName('Marquee__wrapper__content')[0];
+    _currSong = (_currSong) ? _currSong : document.getElementsByClassName('Marquee__wrapper__content__child')[0];
     _currSongTitle = (_currSong) ? _currSong.innerText : 'ADPLAYED';
+    console.log('curr song is: ' + _currSongTitle);
     _intervalId = setInterval(function() {
+        console.log('checking');
         _checkSongTitle = document.getElementsByClassName('Marquee__wrapper__content')[0];
+        _checkSongTitle = (_checkSongTitle) ? _checkSongTitle : document.getElementsByClassName('Marquee__wrapper__content__child')[0];
+        console.log(_checkSongTitle);
         if (_checkSongTitle && _currSongTitle !== _checkSongTitle.innerText) {
-            clearInterval(_intervalId);
+            console.log('should mute');
             document.getElementsByClassName('VolumeDurationControl__VolumeIconContainer')[0].click();
+            clearInterval(_intervalId);
         }
     }, 1000);
 })();
