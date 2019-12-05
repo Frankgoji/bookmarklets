54e0b58b9c7c6d9e6eb9d409140630de235e62ac
Franklin Lee <frankgoji@gmail.com>
2019-12-04 18:24:42 -0800

    Refactor and also correct the behavior for the full page advertisements

--- muteUntil.js
+++ muteUntil.js
@@ -1,17 +1,27 @@
 javascript: (function() {
-    document.getElementsByClassName('VolumeDurationControl__VolumeIconContainer')[0].click();
-    _currSong = document.getElementsByClassName('Marquee__wrapper__content')[0];
-    _currSong = (_currSong) ? _currSong : document.getElementsByClassName('Marquee__wrapper__content__child')[0];
-    _currSongTitle = (_currSong) ? _currSong.innerText : 'ADPLAYED';
+    var clickMuteButton = function() {
+        document.getElementsByClassName('VolumeDurationControl__VolumeIconContainer')[0].click();
+    };
+    var getSongTitle = function() {
+        let checkAdOverlay = document.getElementsByClassName('Video__Top__Message')[0];
+        if (checkAdOverlay && checkAdOverlay.innerText === 'Advertisement') {
+            return 'ADPLAYED';
+        }
+        let currSong = document.getElementsByClassName('Marquee__wrapper__content')[0];
+        currSong = (currSong) ? currSong : document.getElementsByClassName('Marquee__wrapper__content__child')[0];
+        return (currSong) ? currSong.innerText : 'ADPLAYED';
+    };
+    console.log('first click mute');
+    clickMuteButton();
+    _currSongTitle = getSongTitle();
     console.log('curr song is: ' + _currSongTitle);
     _intervalId = setInterval(function() {
-        console.log('checking');
-        _checkSongTitle = document.getElementsByClassName('Marquee__wrapper__content')[0];
-        _checkSongTitle = (_checkSongTitle) ? _checkSongTitle : document.getElementsByClassName('Marquee__wrapper__content__child')[0];
+        console.log(`checking (interval ID ${_intervalId})`);
+        _checkSongTitle = getSongTitle();
         console.log(_checkSongTitle);
-        if (_checkSongTitle && _currSongTitle !== _checkSongTitle.innerText) {
-            console.log('should mute');
-            document.getElementsByClassName('VolumeDurationControl__VolumeIconContainer')[0].click();
+        if (_currSongTitle !== _checkSongTitle) {
+            console.log('should click mute');
+            clickMuteButton();
             clearInterval(_intervalId);
         }
     }, 1000);
