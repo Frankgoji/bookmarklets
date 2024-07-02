e3e6c75247ad0d1087c9a3d0db0ae2e6a6a0e7a3
Franklin Lee <frankgoji@gmail.com>
2024-07-02 00:54:47 -0700

    In case of multiple videos, select the right one

--- youtubeSkip.js
+++ youtubeSkip.js
@@ -1,10 +1,16 @@
 // Copy below for basically a youtube skip button
 javascript: (
     () => {
+        const getVid = () => {
+            return document.getElementById('movie_player').querySelector('video');
+        };
+        if (!document.querySelector('.ytp-ad-text')) {
+            return;
+        }
         try {
             document.getElementsByClassName('ytp-ad-skip-button-modern')[0].click();
         } catch {
-            const v = document.querySelector('video');
+            const v = getVid();
             v.currentTime = v.duration;
             document.getElementsByClassName('ytp-ad-skip-button-modern')[0].click();
         }
@@ -14,6 +20,9 @@ javascript: (
 // Copy below for something that will skip for basically the whole video
 javascript: (
     async () => {
+        const getVid = () => {
+            return document.getElementById('movie_player').querySelector('video');
+        };
         const skip = async () => {
             if (!document.querySelector('.ytp-ad-text')) {
                 return;
@@ -21,7 +30,7 @@ javascript: (
             try {
                 document.getElementsByClassName('ytp-ad-skip-button-modern')[0].click();
             } catch {
-                const v = document.querySelector('video');
+                const v = getVid();
                 if (v.duration < 60) {
                     v.currentTime = v.duration;
                     document.getElementsByClassName('ytp-ad-skip-button-modern')[0].click();
@@ -31,11 +40,11 @@ javascript: (
         };
         await skip(); await skip();
         await new Promise(r => setTimeout(r, 1000));
-        const v = document.querySelector('video');
+        const v = getVid();
         var currVid = window.location.href;
         var normalDuration = v.duration;
         setInterval(() => {
-            const v = document.querySelector('video');
+            const v = getVid();
             if (window.location.href !== currVid) {
                 currVid = window.location.href;
                 normalDuration = v.duration;
