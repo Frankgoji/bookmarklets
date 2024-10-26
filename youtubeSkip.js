6551b0c8a0a7a6370727a0321d7d7b34bc120658
Franklin Lee <frankgoji@gmail.com>
2024-10-26 16:57:03 -0700

    Simplify Youtube skipper

--- youtubeSkip.js
+++ youtubeSkip.js
@@ -24,29 +24,15 @@ javascript: (
                 return;
             }
             const v = getVid();
-            if (v.duration < 60) {
-                v.currentTime = v.duration;
-                document.getElementsByClassName('ytp-skip-ad-button')[0].click();
-            }
+            v.currentTime = v.duration;
+            document.getElementsByClassName('ytp-skip-ad-button')[0].click();
             await new Promise(r => setTimeout(r, 1000));
         };
         await skip(); await skip();
         await new Promise(r => setTimeout(r, 1000));
         const v = getVid();
-        var currVid = window.location.href;
-        var normalDuration = v.duration;
         setInterval(() => {
-            const v = getVid();
-            if (window.location.href !== currVid) {
-                currVid = window.location.href;
-                normalDuration = v.duration;
-            }
-            if (document.getElementsByClassName('ytp-skip-ad-button')[0]) {
-                document.getElementsByClassName('ytp-skip-ad-button')[0].click();
-            }
-            if (v.duration < normalDuration) {
-                skip();
-            }
+            skip();
         }, 200);
     }
 )()
